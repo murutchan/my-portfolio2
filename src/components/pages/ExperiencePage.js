4e5215a865472f9f69b7dc76e9496b8aba5ff872
Askar Talantbek <murutchan@gmail.com>
2021-02-20 17:12:54 -0600

    skills

--- src/components/pages/ExperiencePage.js
+++ src/components/pages/ExperiencePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import HomePage from "./HomePage";
 
 const ExperiencePage = () => {
   return (
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import MainPage from "../MainPage";
 import About from "../About";
-import Skills from "../Skills";
+import Skills from "../../components/Skills";
 import Experience from "../Experience";
 
 import ContactForm from "../ContactForm";
