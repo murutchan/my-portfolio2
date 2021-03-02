f2e9945ee3aed31b92345f86b74214db2ba85e22
Askar Talantbek <murutchan@gmail.com>
2021-03-02 12:09:12 -0600

    some changes

--- src/components/pages/PortfolioPage.js
+++ src/components/pages/PortfolioPage.js
@@ -13,16 +13,6 @@ const PortfolioPage = () => {
           </li>
         </ul>
 
-        <div className="pf-filter-wrapper mb-4">
-          <select className="portfolio-filter-mobile">
-            <option value="*">Everything</option>
-            <option value=".creative">Creative</option>
-            <option value=".video">Video</option>
-            <option value=".design">Design</option>
-            <option value=".branding">Branding</option>
-          </select>
-        </div>
-
         <div className="row portfolio-wrapper">
           <div className="col-md-6 mx-auto col-sm-12 grid-item video mt-5">
             <a href="http://nazbar.netlify.app" target="blank">
@@ -54,21 +44,33 @@ const PortfolioPage = () => {
               <strong>Backend:</strong> Node, express, mongodb, jwt, bcrypt
             </p>
           </div>
-
-          <div className="col-md-4 col-sm-6 grid-item creative design">
-            <a href="http://spygame.netlify.app">
+        </div>
+        <div className="row portfolio-wrapper">
+          <div className="col-md-6 mx-auto col-sm-12 grid-item video mt-5">
+            <a href="http://spygame.netlify.app" target="blank">
               <div className="portfolio-item">
                 <div className="details">
-                  <h4 className="title">Small Game</h4>
-                  <span className="term">Spy Game</span>
+                  <h4 className="title">Spygame</h4>
+                  <span className="term">group game</span>
                 </div>
-                <span className="plus-icon">SPY</span>
+                <span className="plus-icon">Spy</span>
                 <div className="thumb">
-                  <h1>Spy game</h1>
+                  <img src={nazImg} alt="Portfolio-title" />
+
                   <div className="mask"></div>
                 </div>
               </div>
             </a>
+          </div>
+          <div className="col-md-6 col-sm-6 grid-item creative design">
+            <a href="http://spygame.netlify.com">
+              {" "}
+              <h4 className="title text-primary">www.spygame.com</h4>
+            </a>
+            It's a small game for groups up to 10-12 people. I made this game
+            for my friends. We have dinners every 3 weeks, so I thought we need
+            a game where we could just sit and play. This game gives us a lot of
+            laughters and joy. You can try to play too.
           </div>
         </div>
       </section>
