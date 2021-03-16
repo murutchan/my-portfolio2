9d065314eba5cc34263a88a12c97fe8d1de423ae
Askar <murutchan@gmail.com>
2021-03-16 17:07:13 -0500

    story part

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -23,11 +23,47 @@ const AboutPage = () => {
               president Askar Akaev. So the name “Askar” was a good name till
               2005. Because in 2005 there was a revolution and our first
               president escaped to Russia. Since then probably people dont give
-              this name to their children. Briefly about my life before
-              immigration to USA. I went to Turkish high school. I graduated
-              from highschool with all “A”s. After I studied finance in a
-              university. And a little after graduating, I came to US.
+              this name to their children.
+              <br />
+              Briefly about my life before immigration to USA. I went to Turkish
+              high school. I graduated from highschool with all “A”s. After I
+              studied finance in the National university. And a little after
+              graduating, I came to US.
             </p>
+            <h3>Taxi driver in Chicago...</h3>
+            <p className="mb-0 text-dark">
+              I started working as a taxi driver in Chicago. Believe it or not,
+              I never drove a car before a taxi. So imagine passing the taxi
+              written exam and driving exam. I used to take a blue line train to
+              Olive Harvey city college, where I studied for the taxi license.
+              Train used to take 2 hours one way, with changing blue to red line
+              in downtown. FYI: to pass the exam, you need to memorize all the
+              streets in Chicago, Points of interest, rules and regulations etc.
+              Me being from a small village, it was a big challenge. But with
+              perseverence, consistency and hard work I got my license from the
+              second try. I was one answer short in the first try.
+              <br />
+              In my first day shift, I gave all my rides for free, because
+              learning from a book and actual driving was waaaaay different. So
+              I used to ask for direction, and people were nice. I am very
+              thankful for those wonderful people for giving encouragement. I
+              believe, as a stranger the best thing you can do is encourage
+              people.I remember one of my customers words: "You did a great job,
+              my young friend. You will be OK, dont worry" and gave me 20$ tip.
+              And this is just one of many who said a lot of nice words. One
+              interesting thing in the taxi, is you meet a lot of different
+              people. So I decided to ask for advice from my passengers.
+              Everytime I asked for advice, I asked for business card. I
+              collected business cards in my notebook and wrote their stories
+              and advices. Here are some of them:
+            </p>
+            <h3>Banking...</h3>
+            <p className="mb-0 text-dark">
+              As a personal banker I learnt the essence of the customer service.
+              I really enjoyed working with my colleagues. I learnt a lot of new
+              things and met wonderful people.
+            </p>
+            <p>Rest of the story coming soon...</p>
           </div>
         </div>
       </section>
